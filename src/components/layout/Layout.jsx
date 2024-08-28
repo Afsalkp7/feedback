559290a70fdbfef51a68fae024220dae22c22140
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-08-28 16:54:08 +0530

    color changed

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from '../common/header/Navbar'
+import Navbar from '../usual/header/navbar'
 
 const Layout = () => {
   return (
     <>
-    <Navbar/>
+    <Navbar />
     <Outlet />
     </>
   )
